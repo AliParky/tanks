4e5e03d0aa8b01c3c10de3bd4c9e131cef6b0d0d
AliParky <aparkinson670@gmail.com>
2024-01-06 20:11:26 +0800

    Add basic tank rendering

--- main.py
+++ main.py
@@ -6,6 +6,15 @@ screen = pygame.display.set_mode((1280,720))
 
 clock = pygame.time.Clock()
 
+class Tank:
+    def __init__(self, x, y, size):
+        self.rect = pygame.Rect(x, y, size, size)
+
+    def draw(self, screen):
+        pygame.draw.rect(screen, (255, 0, 0), self.rect)
+
+tank = Tank(50, 50, 50)
+
 while True:
     # Process player inputs.
     for event in pygame.event.get():
@@ -19,7 +28,7 @@ while True:
     screen.fill("purple")  # Fill the display with a solid color
 
     # Render the graphics here.
-    # ...
-
+    tank.draw(screen)
+    
     pygame.display.flip()  # Refresh on-screen display
     clock.tick(60)         # wait until next frame (at 60 FPS)
