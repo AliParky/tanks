5e677954ce85afb6ba3ed01b90e7cafa5f7895cd
AliParky <aparkinson670@gmail.com>
2024-01-21 21:20:38 +0800

    Revert "Add angle variable to tank"
    This reverts commit df5218c23a666e30f2b97654aeae67b60ee2e02d.

--- main.py
+++ main.py
@@ -40,7 +40,6 @@ class Tank:
     def __init__(self, x, y, size):
         self.rect = pygame.Rect(x, y, size, size)
         self.speed = 5
-        self.angle = 0
 
     def draw(self, screen):
         pygame.draw.rect(screen, (255, 0, 0), self.rect)
@@ -50,13 +49,14 @@ class Tank:
         original_y = self.rect.y
 
         keys = pygame.key.get_pressed()
-
         if keys[pygame.K_LEFT]:
-            self.angle += 5
+            self.rect.x -= self.speed
         if keys[pygame.K_RIGHT]:
-            self.angle -= 5
-
-        print(self.angle)
+            self.rect.x += self.speed
+        if keys[pygame.K_UP]:
+            self.rect.y -= self.speed
+        if keys[pygame.K_DOWN]:
+            self.rect.y += self.speed
 
         for wall in walls:
             if self.rect.colliderect(wall):
